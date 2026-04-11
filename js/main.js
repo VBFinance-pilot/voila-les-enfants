8ae339a423c6e76b2e822347e15267a9176c5e61
VBFinance-pilot <victor.beauvisage@protonmail.com>
2026-04-12 03:26:36 +0900

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-console.log("🚀 Application modulaire initialisée");
-
-const App = {
-  init: () => console.log("✅ App prête")
-};
-
-document.addEventListener("DOMContentLoaded", App.init);
--- js/modules/lang.js
+++
@@ -1,7 +0,0 @@
-const LangModule = (() => {
-  const switchLang = (lang) => {
-    console.log("Langue changée :", lang);
-    localStorage.setItem("lang", lang);
-  };
-  return { switchLang };
-})();
